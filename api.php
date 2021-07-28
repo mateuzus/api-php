6c3ca41626bc3cafed58fb615b2bfbbc51ae681e
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2021-07-28 16:08:57 -0300

    add JSON_HEX_TAG in json_encode

--- api.php
+++ api.php
@@ -1,10 +1,12 @@
+<pre>
 <?php
 
 $url = "http://35.175.118.233:3000/api/hello";
 $api = json_decode(file_get_contents($url), true);
 $videoId = implode($api);
-$embed = "<iframe width=\"600\" height=\"450\" src=\"https://www.youtube.com/embed/$videoId?feature=oembed&amp;rel=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+$embed = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/$videoId?autoplay=1&controls=0&feature=oembed&amp;rel=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
 $cn_embed = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/$videoId?autoplay=1&controls=0&feature=oembed&amp;rel=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+
 $android = $videoId;
 $ios = $videoId;
 
@@ -14,9 +16,9 @@ $arr = [
     'embed' => $embed,
     'cn_embed' => $cn_embed,
     'streaming_android' => $android,
-    'streaming_ios' => $ios
+    'streaming_ios' => $ios,
+    'name' => 'youtube'
 ];
 
-echo json_encode($arr);
+echo json_encode($arr, JSON_HEX_TAG);
 
-?>
